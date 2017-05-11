fbc7f9af3c2d3049e8431dcbe091525bcba93768
J. Antonio Salomon <josesalomon@gmail.com>
2017-05-11 18:58:09 -0500

    68%

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,9 +1,9 @@
 class Actor < ApplicationRecord
 
   #  - name: must be present; must be unique in combination with dob
-  validate :name, :presence => true, :uniqueness => ({:scope=> :dob})
+  validates :name, :presence => true, :uniqueness => {:scope=> :dob}
 
-  #  - dob: no rules
+    #  - dob: no rules
   #  - bio: no rules
   #  - image_url: no rules
 
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,9 @@
 class Character < ApplicationRecord
 
   #  - movie_id: must be present
-  validate :movie_id, :presence => true
+  validates :movie_id, :presence => true
   #  - actor_id: must be present
-  validate :actor_id, :presence => true
+  validates :actor_id, :presence => true
 
   #  - name: no rules
 
